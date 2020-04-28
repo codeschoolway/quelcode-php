42b58459c2e3f22d995ace9f296d27a5bef76e41
horik <codeschoolway@gmail.com>
2020-04-28 15:03:19 +0900

    初回提出

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,6 +3,16 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
+    
+    $min = $i;
+    for($j=$i+1; $j<count($array); $j++) {
+        if ($array[$min] > $array[$j]) {
+            $min = $j;
+        }
+    }
+    $tmp = $array[$i];
+    $array[$i] = $array[$min];
+    $array[$min] = $tmp;
 
 }
 // 修正はここまで
